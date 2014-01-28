40c173d47def611e2b2326ff006caddae3037cbd
Scott Bailey <csb5t@virginia.edu>
2014-01-28 18:55:11 -0500

    Added in custom queries for my two custom post types, games and moves, into the index.php file. I still need to work out the template structure to move these to the appropriate places in the theme.

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+<?php 
+
+$query_moves = new WP_Query( 'post_type=moves' );
+
+if ( $query_moves->have_posts()) : while( $query_moves->have_posts()) : $query_moves->the_post(); ?>
+<article>
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <?php the_content(); ?>
+</article>
+
+<?php endwhile; else : ?>
+<p>OMG NO POSTS!!!!!</p>
+<?php endif; ?>
+
+<?php 
+
+$query_games = new WP_Query( 'post_type=games' );
+
+if ( $query_games->have_posts()) : while( $query_games->have_posts()) : $query_games->the_post(); ?>
 <article>
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?php the_content(); ?>
